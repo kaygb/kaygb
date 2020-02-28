a070fc62158f301e7c3cda8e52d3f5f14fef151c
kaygb <48642860+kaygb@users.noreply.github.com>
2020-02-28 19:48:12 +0800

    update

---
+++ main.js
@@ -0,0 +1,20 @@
+// addFadeInBackground('https://tva3.sinaimg.cn/large/0072Vf1pgy1foxkjbqfwjj31hc0u0000.jpg', 'background');
+
+// function addFadeInBackground(url, domId) {
+//     var background = new Image();
+//     background.src = url;
+//     background.onload = function() {
+//         console.log('Background load complete!');
+//         var loadbackground = document.getElementById(domId);
+//         loadbackground.style.background = 'url(' + background.src + ')';
+//         loadbackground.style.animationName = 'fadein';
+//     }
+// }
+// JS监听设置网站背景
+window.onload = function() {
+    setTimeout(function() {
+
+        document.body.style.background = "url(https://tva3.sinaimg.cn/large/0072Vf1pgy1foxkjbqfwjj31hc0u0000.jpg)";
+    }, 2e3);
+
+}
