c19e58c2806267d635168adefc10370d16e09999
kaygb <48642860+kaygb@users.noreply.github.com>
2020-03-19 09:36:30 +0800

    Update main.js

--- v2/main.js
+++ v2/main.js
@@ -1,3 +1,8 @@
+//音乐设置
+let music_id = "1413325844";
+let music_name = "Black Catcher";
+let music_artist = "ビッケブランカ";
+
 // var jsonurl = "https://v1.itooi.cn/netease/song?id=1413325844";
 // $.get(jsonurl, function(data) {
 //     alert(data);
@@ -8,17 +13,20 @@ function read() {
     console.log(json.songs.al.name); //输出键为“cc”所对应的值
 }
 // read();
-
-
+let apname = music_name;
+let apart = music_artist;
+let apurl = 'https://v1.itooi.cn/netease/url?id=' + music_id + '&quality=flac';
+let apcover = 'https://v1.itooi.cn/netease/pic?id=' + music_id;
+let aplrc = 'https://v1.itooi.cn/netease/lrc?id=' + music_id;
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     audio: [{
-        name: 'Black Catcher',
-        artist: 'ビッケブランカ',
-        url: 'https://v1.itooi.cn/netease/url?id=1413325844&quality=flac',
-        cover: 'https://v1.itooi.cn/netease/pic?id=1413325844',
-        lrc: 'https://v1.itooi.cn/netease/lrc?id=1413325844',
+        name: apname,
+        artist: apart,
+        url: apurl,
+        cover: apcover,
+        lrc: aplrc,
 
     }],
     autoplay: true
