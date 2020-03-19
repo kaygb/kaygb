68a28bc3f841119e0494ada1ea359ee7e915e84c
kaygb <48642860+kaygb@users.noreply.github.com>
2020-03-19 10:14:10 +0800

    Update main.js

--- v2/main.js
+++ v2/main.js
@@ -31,3 +31,4 @@ const ap = new APlayer({
     }],
     autoplay: true
 });
+console.log('\n' + ' %c PersonalWebSite by 风也温柔 | KAYGB ' + ' %c https://eas1.cn/ ' + '\n' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
