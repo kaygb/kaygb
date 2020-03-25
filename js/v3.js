ec4ebd52d116417683ccbf0a07b535db2a11b44a
kaygb <48642860+kaygb@users.noreply.github.com>
2020-03-25 16:27:04 +0800

    update

--- js/v3.js
+++ js/v3.js
@@ -1,2 +1,7 @@
+var windowWidth = $(window).width();
+if (windowWidth < 767) {
+    // do something
+    $("#aplayer").remove();
+}
 var meting_api = 'https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
 console.log('\n' + ' %c PersonalWebSite by 风也温柔 ' + ' %c https://eas1.cn/posts/165.html ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
