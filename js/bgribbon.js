e12b71a39b129f3997963b35fc2e5b5853f36ab5
kaygb <kaygbtop@163.com>
2020-05-31 13:31:57 +0800

    up

--- js/bgribbon.js
+++ js/bgribbon.js
@@ -209,8 +209,8 @@
                	this._canvas.style["top"] = "0";
                	this._canvas.style["width"] = "100%";
                	this._canvas.style["height"] = "100%";
-                this._canvas.style["z-index"] = "-1";
-				this._canvas.style["background-color"]="#1f1f1f";
+                this._canvas.style["z-index"] = "-2";
+				this._canvas.style["background-color"]="";
                 this._canvas.id = "bgCanvas";	
                 this._onResize();
 
