cb8c16a687e69d3570cff646df70de8048710ae3
kaygb <kaygb@qq.com>
2021-12-12 20:09:47 +0800

    update

--- js/v3.js
+++ js/v3.js
@@ -4,4 +4,4 @@ if (windowWidth < 767) {
     $("#aplayer").remove();
 }
 var meting_api = 'https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
-console.log('\n' + ' %c PersonalWebSite by kaygb ' + ' %c https://blog.kaygb.com/165.html ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
+console.log('\n' + ' %c PersonalWebSite by kaygb ' + ' %c https://www.vwmwv.cn/ ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
