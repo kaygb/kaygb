5771bb82ff92ec3c455916bf0d4936d7f283d0c5
kaygb <kaygb@qq.com>
2021-12-15 17:23:50 +0800

    Update v3.js

--- js/v3.js
+++ js/v3.js
@@ -4,4 +4,4 @@ if (windowWidth < 767) {
     $("#aplayer").remove();
 }
 var meting_api = 'https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
-console.log('\n' + ' %c PersonalWebSite by kaygb ' + ' %c https://www.vwmwv.cn/ ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
+console.log('\n' + ' %c kaygb-v3 by kaygb ' + ' %c https://www.vwmwv.cn/archives/25/ ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
