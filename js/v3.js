dc5b25c2c78163ec107986b4213eb610d0b0506a
kaygb <48642860+kaygb@users.noreply.github.com>
2022-03-16 17:59:24 +0800

    Update v3.js

--- js/v3.js
+++ js/v3.js
@@ -4,4 +4,4 @@ if (windowWidth < 767) {
     $("#aplayer").remove();
 }
 var meting_api = 'https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
-console.log('\n' + ' %c kaygb-v3 by kaygb ' + ' %c https://www.vwmwv.cn/archives/25/ ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
+console.log('\n' + ' %c kaygb-v3 by kaygb ' + ' %c https://170601.xyz/archives/25.html ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
