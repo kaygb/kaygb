9869cdceef2b283db12e6cdd79ed64065ac5a0b4
kaygb <kaygb@qq.com>
2022-03-28 16:28:10 +0800

    Update v3.js

--- js/v3.js
+++ js/v3.js
@@ -1,6 +1,3 @@
 var windowWidth = $(window).width();
-// if (windowWidth < 767) {
-//     $("#aplayer").remove();
-// }
 var meting_api = 'https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
 console.log('\n' + ' %c kaygb-v3 by kaygb ' + ' %c https://blog.170601.xyz/archives/25.html ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
