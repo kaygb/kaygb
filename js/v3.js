754c79aebb8a6b4a4945e74b5c3d66aaebe0b984
kaygb <kaygb@qq.com>
2022-03-29 11:47:27 +0800

    Update v3.js

--- js/v3.js
+++ js/v3.js
@@ -1,3 +1,4 @@
 var windowWidth = $(window).width();
 var meting_api = 'https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
 console.log('\n' + ' %c kaygb-v3 by kaygb ' + ' %c https://blog.170601.xyz/archives/25.html ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
+console.log("新版KZHomePage已经准备就绪，欢迎使用体验：https://github.com/kaygb/KZHomePage ")
